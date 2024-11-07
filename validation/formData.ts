0cd371182571bd110533634dfd33afa8ee9e2bd6
Daniel Sarkodie <dsarkodienyarko@gmail.com>
2024-11-07 23:17:57 +0000

    regex for contact updated

--- validation/formData.ts
+++ validation/formData.ts
@@ -5,12 +5,12 @@ export const formValidation = Joi.object({
   senderNetwork: Joi.string().required().empty(""),
   senderNumber: Joi.string()
     .required()
-    .regex(/^(054|024|055|059|020|050|057|027)/)
+    .regex(/^(054|024|055|059|020|050|057|027)\d*$/)
     .empty("")
     .length(10),
   receiverNetwork: Joi.string().required().empty(""),
   receiverNumber: Joi.string()
-    .regex(/^(054|024|055|059|020|050|057|027)/)
+    .regex(/^(054|024|055|059|020|050|057|027)\d*$/)
     .required()
     .empty("")
     .length(10),
