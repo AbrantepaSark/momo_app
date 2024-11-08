fa80c555a17e6e8102d57be1606707ceccfcc8f8
Daniel Sarkodie <dsarkodienyarko@gmail.com>
2024-11-08 07:15:25 +0000

    add new AT number format

--- validation/formData.ts
+++ validation/formData.ts
@@ -5,12 +5,12 @@ export const formValidation = Joi.object({
   senderNetwork: Joi.string().required().empty(""),
   senderNumber: Joi.string()
     .required()
-    .regex(/^(054|024|055|059|020|050|057|027)\d*$/)
+    .regex(/^(054|024|055|059|020|050|057|027|026|056)\d*$/)
     .empty("")
     .length(10),
   receiverNetwork: Joi.string().required().empty(""),
   receiverNumber: Joi.string()
-    .regex(/^(054|024|055|059|020|050|057|027)\d*$/)
+    .regex(/^(054|024|055|059|020|050|057|027|026|056)\d*$/)
     .required()
     .empty("")
     .length(10),
